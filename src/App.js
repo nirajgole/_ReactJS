e637a8a30973e9f591113b9742a89a7636718c41
nirajgole <nirajgole@protonmail.ch>
2021-04-08 22:05:12 +0530

    added AddUser, DeleteUser and ViewUser components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Contact from './pages/contact';
 import Navbar from './layout/navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NotFound from './pages/error-page/error-page';
+
+import ViewUser from './components/users/ViewUser';
+import AddUser from './components/users/AddUser';
+import EditUser from './components/users/EditUser';
 function App() {
   return (
     <Router>
@@ -14,6 +18,9 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
           <Route exact path='/contact' component={Contact} />
+          <Route exact path='/users/add' component={AddUser} />
+          <Route exact path='/users/edit/:id' component={EditUser} />
+          <Route exact path='/users/:id' component={ViewUser} />
           <Route component={NotFound} />
         </Switch>
       </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await Axios.get('http://localhost:3003/users');
+    setUsers(result.data.reverse());
+  };
+
+  const deleteUser = async id => {
+    await Axios.delete(`http://localhost:3003/users/${id}`);
+    loadUsers();
+  };
+  console.log(users);
   return (
     <div className='container'>
       <h1 className='py-4'>Inside home page</h1>
+      <table className='table'>
+        <thead className='thead-dark'>
+          <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>Name</th>
+            <th scope='col'>User Name</th>
+            <th scope='col'>Email</th>
+            <th scope='col'>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <th scope='row'>{index + 1}</th>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>
+                <Link className='btn btn-primary mr-2' to={`users/${user.id}`}>
+                  View
+                </Link>
+                <Link
+                  className='btn btn-outline-primary mr-2'
+                  to={`users/edit/${user.id}`}
+                >
+                  Edit
+                </Link>
+                <Link
+                  className='btn btn-danger mr-2'
+                  onClick={() => deleteUser(user.id)}
+                >
+                  Delete
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
